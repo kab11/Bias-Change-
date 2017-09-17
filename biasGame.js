2c151f58c3988568ac64fc9f99f0bdf720b26993
kab11 <kab2634@gmail.com>
2017-09-17 10:25:01 -0500

    New game layout with button click

--- biasGame.js
+++ biasGame.js
@@ -5,12 +5,24 @@ var answerChoice = [
 	"No",
 	"Depends"
 ]
+var questions = [
+	"This is a question. Please choose your answer wisely",
+	"This is a second question",
+	"This is a third question"
+]
 
-var random = Math.floor(Math.random() * answerChoice.length);
-var answerResult = answerChoice[random];
+// var random = Math.floor(Math.random() * answerChoice.length);
+// var answerResult = answerChoice[random];
 
+// messageDisplay.textContent = answerResult;
 
+var questionDisplay = document.getElementById("questionDisplay");
 
-messageDisplay.textContent = answerResult;
+var random = Math.floor(Math.random() * questions.length);
+var questionResult = questions[random];
 
+questionDisplay.textContent = questionResult;
 
+document.addEventListener("click", function(){
+	
+})
